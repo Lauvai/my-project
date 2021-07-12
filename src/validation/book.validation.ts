2ae232be7ea490dd2dbc6a667dfa729ba2b2a85b
Lauvai <73236987+Lauvai@users.noreply.github.com>
2021-07-12 15:36:19 +0300

    Add files via upload

---
+++ src/validation/book.validation.ts
@@ -0,0 +1,14 @@
+import Joi from 'joi';
+
+const validateSchema = Joi.object ({
+    id : Joi.number().required(),
+    bookName : Joi.string().min(1).max(100).required(),
+    bookAuthor : Joi.string().min(1).max(100).required(),
+    bookGenre : Joi.string().min(1).max(100).required(),
+    bookPage : Joi.number().required(),
+    bookPrice : Joi.number().required()
+ 
+
+})
+
+export default validateSchema;
